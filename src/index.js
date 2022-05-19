2f239b9c27ebbc42dd1f378c8e0cc07b42853e4f
Wandir Pereira Filho <wandir@gmail.com>
2022-05-18 23:35:23 -0300

    Deletando arquivos desnecessários

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//console.log('Hola Mundo');
+//import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const saludo = <h1> Hola Mundo</h1>;
+console.log(saludo);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//const divRoot = document.querySelector('#root');
+//console.log(divRoot);
+const root=createRoot(document.querySelector('#root'));
+root.render(saludo);
+
+//const saludo = <h1> Hola Mundo</h1>;
